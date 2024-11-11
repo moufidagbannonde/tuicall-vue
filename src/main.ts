326b0929ea82833afb5622172733dfe42102f562
Moufid AGBANNONDE <moufid.agbannonde@chem.loc>
2024-11-11 10:01:09 +0100

    group call okay

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import Toast from "primevue/toast";
-import ToastService from "primevue/toastservice";
 
 const app = createApp({});
 createApp(App).mount('#app')
-app.use(ToastService);
